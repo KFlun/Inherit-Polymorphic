6d19c3488b970b7ebe5b74f0d798f983a1f4f678
KnifeofLondon <1628406560@qq.com>
2019-05-16 11:48:14 +0800

    new

--- 几何图形中类的继承关系（纯虚函数）/head.h
+++ 几何图形中类的继承关系（纯虚函数）/head.h
@@ -5,18 +5,21 @@ class Geometric_shape
 {
 public:
 	virtual void show() {};
-	virtual double perimeter() { return 0; };  
-	virtual double area() { return 0; };
-	virtual double volume() { return 0; };       
+	virtual double perimeter() = 0;              //高度抽象类，无法自己实现实例化。抽象类中的函数没有具体含义，它存在的意义就是给子类（派生类）进行重写的
+	//virtual double perimeter() { return 0; };  //且知道 **所有** 纯虚函数被重写之前，抽象类的子类一直保持抽象状态
+												 //在C++中这个知识点叫做纯虚函数，在Java中叫做接口
+	virtual double area() = 0;
+	virtual double volume() = 0;       //当将volume变为纯虚函数的时候，需要在以下继承该抽象类的平面中
 };
 
-class Circle:public Geometric_shape
+class Circle :public Geometric_shape
 {
 public:
 	Circle(double r) :r(r) {};
 	virtual void show() { cout << "构造一个圆"; };
 	virtual double perimeter();
 	virtual double area();
+	virtual double volume() { return 0; }
 private:
 	double r;
 };
@@ -28,6 +31,7 @@ public:
 	virtual void show() { cout << "构造一个矩形"; };
 	virtual double perimeter();
 	virtual double area();
+	virtual double volume() { return 0; }
 private:
 	double length, width;
 };
@@ -39,6 +43,7 @@ public:
 	virtual void show() { cout << "构造一个三角形"; };
 	virtual double perimeter();
 	virtual double area();
+	virtual double volume() { return 0; }
 private:
 	double a, b, c;
 };
@@ -46,7 +51,7 @@ private:
 class Box :public Rectangle
 {
 public:
-	Box(double length, double width, double hight) :Rectangle(length,width), hight(hight) {};
+	Box(double length, double width, double hight) :Rectangle(length, width), hight(hight) {};
 	virtual void show() { cout << "构造一个长方体"; };
 	virtual double volume();
 private:
@@ -76,7 +81,7 @@ private:
 class T_pyramid :public Triangle
 {
 public:
-	T_pyramid(double a, double b, double c, double hight) :Triangle(a,b,c), hight(hight) {};
+	T_pyramid(double a, double b, double c, double hight) :Triangle(a, b, c), hight(hight) {};
 	virtual void show() { cout << "构造一个三棱锥"; };
 	virtual double volume();
 private:
